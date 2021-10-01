56a92a9674c0efadeb468382ed7f7b62689a95ac
Aliakbar2251 <85884214+Aliakbar2251@users.noreply.github.com>
2021-10-01 15:46:08 +0500

    unique

--- database/migrations/2021_09_27_100235_create_genders_table.php
+++ database/migrations/2021_09_27_100235_create_genders_table.php
@@ -14,7 +14,7 @@ class CreateGendersTable extends Migration
     public function up()
     {
         Schema::create('genders', function (Blueprint $table) {
-            $table->string('type')->primary();
+            $table->string('type')->primary()->unique();
             $table->timestamps();
         });
     }
