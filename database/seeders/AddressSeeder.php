8130130750310ff2355294aeb3b551ca13c2467d
Aliakbar2251 <85884214+Aliakbar2251@users.noreply.github.com>
2021-10-02 12:14:35 +0500

    fixing migrations

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -17,10 +17,10 @@ class AddressSeeder extends Seeder
     public function run()
     {
         DB::table('addresses')->insert([
-            'body' => 'Norak',
+            'body' => 'Norak1',
             'is_main' => '1',
             'description' => 'Consectetur quia quisquam itaque amet ut.',
-            'contractor_id' => '2'
+            'contractor_id' => '1'
         ]);
 
     }
